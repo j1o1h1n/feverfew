d750d1c4d8a379c88c21f6ef9b7e2411b724068f
John Lehmann <j1o1h1n@gmail.com>
2020-05-05 18:50:48 +1000

    Flint and steel

--- src/main/java/com/helianthi/feverfew/FeverfewMod.java
+++ src/main/java/com/helianthi/feverfew/FeverfewMod.java
@@ -1,12 +1,7 @@
 package com.helianthi.feverfew;
 
-import com.helianthi.feverfew.items.KeyItem;
-import com.helianthi.feverfew.items.PadlockItem;
+import com.helianthi.feverfew.padlock.Padlock;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,15 +13,11 @@ public class FeverfewMod implements ModInitializer {
     public static final String MOD_ID = "feverfew";
     public static final String MOD_NAME = "Feverfew";
 
-    public static final PadlockItem PADLOCK_ITEM = new PadlockItem(new Item.Settings().group(ItemGroup.TOOLS));
-    public static final KeyItem KEY_ITEM = new KeyItem(new Item.Settings().group(ItemGroup.TOOLS));
 
     @Override
     public void onInitialize() {
         log(Level.INFO, "Initializing " + MOD_NAME);
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "padlock_item"), PADLOCK_ITEM);
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "key_item"), KEY_ITEM);
-        PadlockItem.initialise();
+        Padlock.initialise();
     }
 
     public static void log(Level level, String message){
--- src/main/java/com/helianthi/feverfew/padlock/Padlock.java
+++ src/main/java/com/helianthi/feverfew/padlock/Padlock.java
@@ -1,6 +1,5 @@
-package com.helianthi.feverfew.items;
+package com.helianthi.feverfew.padlock;
 
-import com.helianthi.feverfew.FeverfewMod;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.block.Blocks;
@@ -8,43 +7,51 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.HitResult;
 import org.apache.logging.log4j.Level;
 
-import static com.helianthi.feverfew.FeverfewMod.PADLOCK_ITEM;
-import static com.helianthi.feverfew.FeverfewMod.KEY_ITEM;
 import static com.helianthi.feverfew.FeverfewMod.log;
 
-public class PadlockItem extends Item
-{
+/**
+ * A Flint and Steel may be used to lock a chest.  First, the Flint and Steel
+ * must be named, using an Anvil. Then when the Flint and Steel is used on an
+ * unlocked chest, the flint and steel will be consumed and the chest locked
+ * with the name as a the secret key.
+ *
+ * To open a locked chest the player must be holding an item with the secret name.
+ *
+ * To take the lock off a chest, hit it with an item with the secret name.
+ */
+public abstract class Padlock {
 
-    public PadlockItem(Item.Settings settings) {
-        super(settings);
-    }
+    public static String PADLOCK_UNNAMED = "padlock.unnamed";
+    public static String PADLOCK_REMOVED = "padlock.removed";
+    public static String PADLOCK_LOCKED = "padlock.locked";
+    public static String PADLOCK_SUCCESS = "padlock.success";
+    public static String PADLOCK_BREAK = "padlock.break";
 
     public static void initialise() {
         UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
             if (!player.isSpectator()
-                    && player.getMainHandStack().getItem() == PADLOCK_ITEM
+                    && player.getMainHandStack().getItem() == Items.FLINT_AND_STEEL
                     && hitResult.getType() == HitResult.Type.BLOCK
                     && world.getBlockState(hitResult.getBlockPos()).getBlock() == Blocks.CHEST)
             {
-                // if the padlock has no name, display a failure message
+                // if the Flint and Steel has no name, display a failure message
                 // if the chest is already locked, display failure message
                 // else lock the chest and consume the lock
                 ItemStack stack = player.getMainHandStack();
                 if (!stack.hasCustomName()) {
-                    player.sendMessage(new TranslatableText("padlock.unnamed"), true);
-                    player.playSound(SoundEvents.BLOCK_DISPENSER_FAIL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                    player.sendMessage(new TranslatableText(PADLOCK_UNNAMED), true);
+                    player.playSound(SoundEvents.BLOCK_DISPENSER_FAIL,
+                            SoundCategory.BLOCKS, 1.0F, 1.0F);
                     return ActionResult.FAIL;
                 }
                 if (world.isClient) {
@@ -58,14 +65,14 @@ public class PadlockItem extends Item
                 CompoundTag tag = new CompoundTag();
                 blockEntity.toTag(tag);
                 if (tag.contains("Lock")) {
-                    player.sendMessage(new TranslatableText("padlock.already.locked"), true);
+                    player.sendMessage(new TranslatableText(PADLOCK_LOCKED), true);
                     return ActionResult.FAIL;
                 }
                 String lockName = stack.getName().getString();
                 tag.putString("Lock", lockName);
                 blockEntity.fromTag(blockEntity.getCachedState(), tag);
                 stack.decrement(1);
-                player.sendMessage(new TranslatableText("padlock.success", lockName), true);
+                player.sendMessage(new TranslatableText(PADLOCK_SUCCESS, lockName), true);
                 player.playSound(SoundEvents.BLOCK_CHEST_LOCKED, SoundCategory.BLOCKS, 1.0F, 1.0F);
                 return ActionResult.FAIL;
             }
@@ -78,7 +85,6 @@ public class PadlockItem extends Item
             {
                 // a locked chest cannot be broken, but if attacked with the key the lock will come off
                 if (world.isClient) {
-                    log(Level.WARN, "cannot determine if chest is locked from client");
                     return ActionResult.PASS;
                 }
                 BlockEntity blockEntity = world.getBlockEntity(pos);
@@ -96,20 +102,21 @@ public class PadlockItem extends Item
 
                 // a padlocked chest cannot be broken, but if the player has the right key, the lock will come off
                 ItemStack stack = player.getMainHandStack();
-                if (stack.getItem() == KEY_ITEM && stack.hasCustomName() && stack.getName().asString().equals(lockName)) {
+                if (stack.hasCustomName()
+                        && stack.getName().asString().equals(lockName)) {
                     tag.remove("Lock");
-                    player.sendMessage(new TranslatableText("padlock.removed"), true);
+                    player.sendMessage(new TranslatableText(PADLOCK_REMOVED), true);
                     blockEntity.fromTag(blockEntity.getCachedState(), tag);
 
-                    // create padlock at the chest position
-                    ItemStack padlock = new ItemStack(PADLOCK_ITEM);
+                    // create Flint and Steel at the chest position
+                    ItemStack padlock = new ItemStack(Items.FLINT_AND_STEEL);
                     ItemEntity itemEntity = new ItemEntity(player.world, pos.getX(), pos.getY(), pos.getZ(), padlock);
                     player.world.spawnEntity(itemEntity);
                     return ActionResult.FAIL;
                 }
 
-                player.sendMessage(new TranslatableText("break.locked.chest"), true);
-                player.damage(DamageSource.MAGIC, 2.0f);
+                player.sendMessage(new TranslatableText(PADLOCK_BREAK), true);
+                player.damage(DamageSource.MAGIC, 5.0f);
                 player.setFireTicks(30);
                 player.playSound(SoundEvents.ENTITY_PLAYER_HURT_ON_FIRE, 1.0F, 1.0F);
 
@@ -118,5 +125,4 @@ public class PadlockItem extends Item
             return ActionResult.PASS;
         });
     }
-
 }
